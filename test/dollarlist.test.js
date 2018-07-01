9454d74d838c44c36bf39631ad7e09a7a43bddb9
disinvite <disinvite@users.noreply.github.com>
2018-06-30 21:26:21 -0400

    lorem ipsum test

--- test/dollarlist.test.js
+++ test/dollarlist.test.js
@@ -44,6 +44,14 @@ describe('convert longer strings', () => {
         let data = ' '.repeat(259) + 'x';
         let v0 = [0x0,0x5,0x1,0x1].concat( Array(259).fill(0x20) ).concat(0x78);
 
-        assert.deepEqual($list.convert(v0),[data])
+        assert.deepEqual($list.convert(v0),[data]);
+    });
+
+    it('should lorem ipsum', () => {
+        //445 characters. $lb() length is 4 more than that.
+        const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+        let lb = [0x00,0xbe,0x01,0x01].concat(lorem.split('').map((x) => x.charCodeAt(0)));
+
+        assert.deepEqual($list.convert(lb),[lorem]);
     });
 });
